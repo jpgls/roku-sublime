0f9e53a9a70997fbec2e8f2d42ea2313c5176e0b
jccrosby <john@realeyes.com>
2013-04-24 22:55:56 -0600

    updated to use python script for zip files.

--- roku.py
+++ roku.py
@@ -2,6 +2,7 @@ import sublime, sublime_plugin
 import os, sys
 import subprocess
 import re
+import zipfile
 
 def get_root_directory(self):
 	folders = self.window.folders();
@@ -28,52 +29,79 @@ class RokuPackageCommand(sublime_plugin.WindowCommand):
 		
 		projectDir = get_root_directory(self);
 		projectName = get_project_name(projectDir);
+		outputDirectory = projectDir + os.sep + 'build';
+		ouputZip = outputDirectory + os.sep + projectName;
 		
-		result = subprocess.Popen([
-			'zip', '-r', projectDir + os.sep + projectName + '.zip', 
-			get_root_directory(self), '-x', '*.git*'],
-			stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		# check for and create build directory if necessary
+		if not os.path.exists(outputDirectory):
+			os.makedirs(outputDirectory);
 		
-		output, errors = result.communicate()
+		print('Zipping ' + projectDir);
 		
-		if result.returncode:
-			print(output.decode());
-		else: 
-			print(output.decode());
+		zf = zipfile.ZipFile('%s.zip' % (ouputZip), 'w');
+		abs_src = os.path.abspath(projectDir);
+		
+		for dirname, subdirs, files in os.walk(projectDir):
+			for filename in files:
+				absname = os.path.abspath(os.path.join(dirname, filename));
+				arcname = absname[len(abs_src) + 1:];
+				
+				# messy need to fix this
+				if(arcname.find('.buildpath') == -1 and 
+					arcname.find('.gitignore') == -1 and 
+					arcname.find('.project') == -1 and
+					arcname.find('.git') == -1 and 
+					arcname.find('.sublime') == -1 and
+					arcname.find('build') == -1):
+						print('zipping %s as %s' % (os.path.join(dirname, filename), arcname));
+						zf.write(absname, arcname)
+		zf.close()
+		
+		# result = subprocess.Popen([
+		# 	'zip', '-r', projectDir + os.sep + 'build' + os.sep + projectName + '.zip', 
+		# 	get_root_directory(self), '-x', '*.git*'],
+		# 	stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		
+		# output, errors = result.communicate()
+		
+		# if result.returncode:
+		# 	print(output.decode());
+		# else: 
+		# 	print(output.decode());
 			
 		print('packaging complete.');
 
 class RokuInstallCommand(sublime_plugin.ApplicationCommand):
 	def run(args):
 		print('Installing...');
-		result = subprocess.Popen([
-			'curl', '-F', 
-			'archive=@$1.zip;type=application/zip', 
-			'-F', 'mysubmit=Install', '-H',
-			'Content-type:multipart/form-data', 
-			'192.168.0.1/plugin_install'])
-		output, errors = result.communicate()
-		if result.returncode:
-			raise Exception(errors)
-		else: 
-			print(output);
+		# result = subprocess.Popen([
+		# 	'curl', '-F', 
+		# 	'archive=@$1.zip;type=application/zip', 
+		# 	'-F', 'mysubmit=Install', '-H',
+		# 	'Content-type:multipart/form-data', 
+		# 	'192.168.0.1/plugin_install'])
+		# output, errors = result.communicate()
+		# if result.returncode:
+		# 	raise Exception(errors)
+		# else: 
+		# 	print(output);
 		print('installation complete.');
 
 
 class RokuReplaceCommand(sublime_plugin.ApplicationCommand):
 	def run(args):
-		print('Installing...');
-		result = subprocess.Popen([
-			'curl', '-F', 
-			'archive=@$1.zip;type=application/zip', 
-			'-F', 'mysubmit=Replace', '-H',
-			'Content-type:multipart/form-data', 
-			'192.168.0.1/plugin_install'])
-		output, errors = result.communicate()
-		if result.returncode:
-			raise Exception(errors)
-		else: 
-			print(output);
+		print('Replacing...');
+		# result = subprocess.Popen([
+		# 	'curl', '-F', 
+		# 	'archive=@$1.zip;type=application/zip', 
+		# 	'-F', 'mysubmit=Replace', '-H',
+		# 	'Content-type:multipart/form-data', 
+		# 	'192.168.0.1/plugin_install'])
+		# output, errors = result.communicate()
+		# if result.returncode:
+		# 	raise Exception(errors)
+		# else: 
+		# 	print(output);
 		print('installation complete.');
 
 	
